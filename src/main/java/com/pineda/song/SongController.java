b8a8bcd6eceab8e139d22869f0613fced1d70a91
sm-pineda <sm_pineda.student@ua.edu.ph>
2026-04-06 22:20:54 +0800

    first commit

---
+++ src/main/java/com/pineda/song/SongController.java
@@ -0,0 +1,70 @@
+package com.pineda.song;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pineda/songs")
+public class SongController {
+
+    @Autowired
+    private SongRepository songRepository;
+
+    @GetMapping
+    public List<Song> getAllSongs() {
+        return songRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Song> getSongById(@PathVariable Long id) {
+        Optional<Song> song = songRepository.findById(id);
+        return song.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Song createSong(@RequestBody Song song) {
+        return songRepository.save(song);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Song> updateSong(@PathVariable Long id, @RequestBody Song songDetails) {
+        Optional<Song> optionalSong = songRepository.findById(id);
+
+        if (!optionalSong.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Song song = optionalSong.get();
+        song.setTitle(songDetails.getTitle());
+        song.setArtist(songDetails.getArtist());
+        song.setAlbum(songDetails.getAlbum());
+        song.setGenre(songDetails.getGenre());
+        song.setUrl(songDetails.getUrl());
+
+        Song updatedSong = songRepository.save(song);
+        return ResponseEntity.ok(updatedSong);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSong(@PathVariable Long id) {
+        Optional<Song> song = songRepository.findById(id);
+        if (!song.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        songRepository.delete(song.get());
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/search/{query}")
+    public List<Song> searchSongs(@PathVariable String query) {
+        return songRepository.findByTitleContainingIgnoreCaseOrArtistContainingIgnoreCaseOrAlbumContainingIgnoreCaseOrGenreContainingIgnoreCase(
+                query, query, query, query
+        );
+    }
+}
